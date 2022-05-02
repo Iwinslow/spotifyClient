cc7b6bbd41abfe8c6b991576132067e9f773a860
Ivan Winslow <ivan_winslow@hotmail.com>
2022-05-02 19:29:00 -0300

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import SearchAlbums from "./pages/SearchAlbums";
+
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/search/" element={<SearchAlbums />} />
+        <Route path="/me" element={<Profile />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
